8099ecb16854c5b3ae1a7ad1bdbe1daa28691db8
MiguelCarvalho403 <142493500+MiguelCarvalho403@users.noreply.github.com>
2024-05-28 00:18:25 -0300

    Update coordenate_system.py

--- sistema de coordenadas.py
+++ sistema de coordenadas.py
@@ -1,16 +1,19 @@
-def coordenadas():
+def coordenadas(ax_size = 2, plane = False, opacity = 0.1):
     u, v = var('y, z')
-    u_ = (u, -2, 2)
-    v_ = (v, -2, 2)
+    u_ = (u, -ax_size, ax_size) # dimensão dos eixos / planos
+    v_ = (v, -ax_size, ax_size) # dimensão dos eixos / planos
 
-    #xz = implicit_plot3d(x == 0, (-2, 2), (-2,2), (-2,2), opacity=0.3)
-    yz = parametric_plot((u, v, 0), u_, v_, opacity = 0.1, color='red')
-    xy = parametric_plot((u, 0, v), u_, v_, opacity=0.1, color='yellow')
-    xz = parametric_plot((0, u, v), u_, v_, opacity=0.1)
+    x = parametric_plot((u, 0, 0), u_,  opacity=opacity, color='red')
+    y = parametric_plot((0, u, 0), u_,  opacity=opacity, color='yellow')
+    z = parametric_plot((0, 0, u), u_,  opacity=opacity) # Blue
+    
+    if plane == True:
+        
+        #xz = implicit_plot3d(x == 0, (-2, 2), (-2,2), (-2,2), opacity=0.3)
+        yz = parametric_plot((u, v, 0), u_, v_, opacity=opacity, color='red')
+        xy = parametric_plot((u, 0, v), u_, v_, opacity=opacity, color='yellow')
+        xz = parametric_plot((0, u, v), u_, v_, opacity=opacity)
 
-    x = parametric_plot((u, 0, 0), u_,  opacity = 1, color='red')
-    y = parametric_plot((0, u, 0), u_,  opacity=1, color='yellow')
-    z = parametric_plot((0, 0, u), u_,  opacity=1)
+        return xz +xy + yz + x + y + z # planos
 
     return x + y + z #eixos
-    #return xz +xy + yz # planos
