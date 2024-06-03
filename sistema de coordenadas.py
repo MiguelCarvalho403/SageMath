58e85734604e233e6cd885b794a11b10a3d9cb14
MiguelCarvalho403 <142493500+MiguelCarvalho403@users.noreply.github.com>
2024-06-03 09:37:46 -0300

    Update sistema de coordenadas.py

--- sistema de coordenadas.py
+++ sistema de coordenadas.py
@@ -1,19 +1,32 @@
-def coordenadas(ax_size = 2, plane = False, opacity = 0.1):
+def coordenadas(ax_size = (-2,2), plane = False, opacity = 0.1):
     u, v = var('y, z')
-    u_ = (u, -ax_size, ax_size) # dimensão dos eixos / planos
-    v_ = (v, -ax_size, ax_size) # dimensão dos eixos / planos
+    size = (ax_size[1] - ax_size[0])
+    u_ = (u, ax_size[0], ax_size[1]) # dimensão dos eixos / planos
+    v_ = (v, ax_size[0], ax_size[1]) # dimensão dos eixos / planos
 
     x = parametric_plot((u, 0, 0), u_,  opacity=opacity, color='red')
+    x += vector([ax_size[1],0,0]).plot(color = 'red')
+    labels = text3d('x',1.1*vector([ax_size[1],0,0]),fontsize= str(50*size)+'%')
+    
     y = parametric_plot((0, u, 0), u_,  opacity=opacity, color='yellow')
+    y += vector([0,ax_size[1],0]).plot(color = 'yellow')
+    labels += text3d('y',1.1*vector([0,ax_size[1],0]),fontsize= str(50*size)+'%')
+    
     z = parametric_plot((0, 0, u), u_,  opacity=opacity) # Blue
+    z += vector([0,0,ax_size[1]]).plot(color = 'blue')
+    labels += text3d('z',1.1*vector([0,0,ax_size[1]]),fontsize= str(50*size)+'%')
     
     if plane == True:
         
-        #xz = implicit_plot3d(x == 0, (-2, 2), (-2,2), (-2,2), opacity=0.3)
-        yz = parametric_plot((u, v, 0), u_, v_, opacity=opacity, color='red')
-        xy = parametric_plot((u, 0, v), u_, v_, opacity=opacity, color='yellow')
-        xz = parametric_plot((0, u, v), u_, v_, opacity=opacity)
+        cont = ax_size[0]
+        xy = parametric_plot((u, cont, 0), u_, opacity=opacity, color='black')
+        xy += parametric_plot((cont, u, 0), u_, opacity=opacity, color='black')
+        while cont != ax_size[1]:
 
-        return xz +xy + yz + x + y + z # planos
+            cont += size/10
+            xy += parametric_plot((u, cont, 0), u_, opacity=opacity, color='black')
+            xy += parametric_plot((cont, u, 0), u_, opacity=opacity, color='black')
+        return xy + x + y + z + labels # planos
 
-    return x + y + z #eixos
+    return x + y + z + labels #eixos
+    
