0df7df7ac609118c8ffb556b8a86567229c02ae1
MiguelCarvalho403 <142493500+MiguelCarvalho403@users.noreply.github.com>
2024-06-03 09:40:07 -0300

    Update sistema de coordenadas.py

--- sistema de coordenadas.py
+++ sistema de coordenadas.py
@@ -4,15 +4,15 @@ def coordenadas(ax_size = (-2,2), plane = False, opacity = 0.1):
     u_ = (u, ax_size[0], ax_size[1]) # dimensão dos eixos / planos
     v_ = (v, ax_size[0], ax_size[1]) # dimensão dos eixos / planos
 
-    x = parametric_plot((u, 0, 0), u_,  opacity=opacity, color='red')
+    x = parametric_plot((u, 0, 0), u_,  opacity=1, color='red')
     x += vector([ax_size[1],0,0]).plot(color = 'red')
     labels = text3d('x',1.1*vector([ax_size[1],0,0]),fontsize= str(50*size)+'%')
     
-    y = parametric_plot((0, u, 0), u_,  opacity=opacity, color='yellow')
+    y = parametric_plot((0, u, 0), u_,  opacity=1, color='yellow')
     y += vector([0,ax_size[1],0]).plot(color = 'yellow')
     labels += text3d('y',1.1*vector([0,ax_size[1],0]),fontsize= str(50*size)+'%')
     
-    z = parametric_plot((0, 0, u), u_,  opacity=opacity) # Blue
+    z = parametric_plot((0, 0, u), u_,  opacity=1) # Blue
     z += vector([0,0,ax_size[1]]).plot(color = 'blue')
     labels += text3d('z',1.1*vector([0,0,ax_size[1]]),fontsize= str(50*size)+'%')
     
